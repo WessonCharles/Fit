6ce4e3a98f178c218962fcf270db124344d0a7d7
chqiangs <huiqiang5136@163.com>
2016-07-14 02:10:56 +0800

    modify sidebar styles

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="wrapper">
         <div class="header">
             <div class="logo">
-              <img src="./assets/logo.png" alt="今日训练">
+              <a href="/"><img src="./assets/logo.png" alt="今日训练"></a>
             </div>
             <div class="rightmenu">
               <div class="userinfo">
@@ -26,8 +26,35 @@
               </div>
             </div>
         </div>
-        <div class="container">
-          
+        <div class="containers">
+          <div class="sidebar">
+            <div class="side-title">
+              
+              <a><img src="" alt=""><span>Crossfit PowerSonic</span></a>
+            </div>
+            <div class="side-route">
+              <p class="route-title">训练</p>
+              <ul>
+                <li>
+                  <a href=""><i class="fa fa-dashboard"></i><span>321 GO!</span></a>
+                </li>
+                <li class="active">
+                  <a href=""><i class="fa fa-edit"></i><span>编排</span></a>
+                </li>
+              </ul>
+            </div>
+            <div class="side-route">
+              <p class="route-title">会员</p>
+              <ul>
+                <li>
+                  <a href=""><i class="fa fa-trophy"></i><span>排名</span></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="content">
+            
+          </div>
         </div>
     </div>
 </template>
@@ -38,12 +65,6 @@ import dropdown from './components/Dropdown.vue'
 export default {
   components: {
     dropdown
-  },
-  ready() {
-
-  },
-  data (){
-
   }
 }
 </script>
